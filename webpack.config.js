b0a808cc0ff88b400be499bed0ff164b28a30c6b
D <Di_1@inbox.ru>
2022-08-18 15:53:12 +0300

    fix: add plugins

--- webpack.config.js
+++ webpack.config.js
@@ -35,10 +35,14 @@ module.exports = {
           options: {
             presets: [
               '@babel/preset-env'
+            ],
+            plugins: [
+              "@babel/plugin-proposal-class-properties",
+              "@babel/plugin-proposal-private-methods"
             ]
           }
         }
       }
     ]
-  }
+  },
 };
